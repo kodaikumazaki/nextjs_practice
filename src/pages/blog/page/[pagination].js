5b7845fe98933a625741661a1a157501a2cddc1a
kodaikumazaki <kodai.kumazaki@play-life.jp>
2021-10-02 21:23:50 +0900

    feat: pagination

--- src/pages/blog/page/[pagination].js
+++ src/pages/blog/page/[pagination].js
@@ -1,12 +1,12 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-import * as style from "../styles/blog.module.scss"
-import { getAllBlogs, blogsPerPage } from '../utils/mdQueries'
-import Pagination from '../components/pagination'
+import Layout from "../../../components/layout"
+import Seo from "../../../components/seo"
+import * as style from "../../../styles/blog.module.scss"
+import { getAllBlogs, blogsPerPage } from '../../../utils/mdQueries'
+import Pagination from '../../../components/pagination'
 
-const Blog = ({blogs, numberPages}) => {
+const PaginationPage = ({ blogs, numberPages }) => {
   return (
       <Layout>
           <Seo title="ブログ" description="これはブログページです。" />
@@ -37,15 +37,26 @@ const Blog = ({blogs, numberPages}) => {
     )
 }
 
-export default Blog
+export default PaginationPage
 
-export async function getStaticProps() {
+export async function getStaticPaths() {
+    const { numberPages } = await getAllBlogs()
+    let paths = []
+    Array.from({ length: numberPages }).slice(0, 1).forEach((_, i) => paths.push(`/blog/page/${i + 2}`))
+    return {
+        paths: paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps(context) {
     const {orderedBlogs, numberPages} = await getAllBlogs()
-    const limitedBlogs = orderedBlogs.slice(0, blogsPerPage)
+    const currentPage = context.params.pagination
+    const limitedBlogs = orderedBlogs.slice((currentPage - 1) * blogsPerPage, currentPage * blogsPerPage) 
     return {
         props: {
             blogs: limitedBlogs,
             numberPages: numberPages
-        },
+        }
     }
 }
