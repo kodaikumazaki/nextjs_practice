c29e5762bc2e241df4573a6284f3992ccc002950
kodaikumazaki <kodai.kumazaki@play-life.jp>
2021-10-02 18:35:47 +0900

    add: contact_form and seo and 404pages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,22 +1,24 @@
+import Seo from "next-seo"
 import Layout from "../components/layout"
-import * as style from "../styles/contact.module.scss" 
+import * as style from "../styles/contact.module.scss"
 
 const Contact = () => {
     return (
         <Layout>
+            <Seo title="コンタクト" description="これはコンタクトページです" />
             <div className={style.wrapper}>
                 <div className={style.container}>
                     <h1>Contact</h1>
                     <p>お気軽にご連絡ください</p>
-                    <form>
-                        <label htmlFor="name">お名前</label>  
+                    <form action="https://formspree.io/f/xnqlrdwk" method="POST">
+                        <label htmlFor="name">お名前</label>
                         <input type="text" name="name" id="name" required/>
                         <label htmlFor="email">メールアドレス</label>
                         <input type="email" name="email" id="email" required/>
                         <label htmlFor="textarea">ご用件</label>
                         <textarea name="message" rows="10" id="textarea" required></textarea>
                         <button type="submit">送信</button>
-                    </form> 
+                    </form>
                 </div>
             </div>
         </Layout>
